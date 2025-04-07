3e02647b0c1404b86196d5d6bbed62b004307ee6
Dimitrov Vladislav <64010169+VladislavDim@users.noreply.github.com>
2025-04-07 23:33:50 +0300

    Add Home page

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Features from "../components/features/Features";
+import Hero from "../components/hero/Hero";
+
+export default function Home() {
+    return (
+        <>
+            <Hero />
+            <Features />
+        </>
+    );
+}
