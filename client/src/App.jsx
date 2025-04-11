8482cb02ece4379a84dc3be63deed8cbff51b082
Dimitrov Vladislav <64010169+VladislavDim@users.noreply.github.com>
2025-04-11 21:56:38 +0300

    Refactor App.jsx: organize imports, add route guards, and improve structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,53 +1,61 @@
 import { Routes, Route } from 'react-router'
 
-import UserProvider from "./providers/UserProvider";
-import Footer from './components/footer/Footer'
+import UserProvider from './providers/UserProvider'
+
+import AuthGuard from './guards/AuthGuard'
+import GuestGuard from './guards/GuestGuard'
+
 import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
+
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
+import Logout from './pages/Logout'
+import MyProfile from './pages/MyProfile'
 import Restaurants from './pages/Restaurants'
 import RestaurantDetails from './pages/RestaurantDetails'
-import MyProfile from './pages/MyProfile'
-import Logout from './pages/Logout'
+import AddRestaurant from './pages/AddRestaurant'
+import EditRestaurant from './pages/EditRestaurant'
+import MyRestaurants from './pages/MyRestaurants'
+import NotFound from './pages/NotFound'
 
 import './App.css'
-import AddRestaurant from './pages/AddRestaurant'
-import NotFound from './pages/NotFound'
-import MyRestaurants from './pages/MyRestaurants'
-import EditRestaurant from './pages/EditRestaurant'
 
 function App() {
+  return (
+    <UserProvider>
+      <div className="min-h-screen flex flex-col">
+        <Header />
 
-    return (
-        <UserProvider>
-            <div className="min-h-screen flex flex-col" >
+        <main className="flex-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
 
-                <Header />
+            <Route element={<GuestGuard />}>
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+            </Route>
 
-                <main className="flex-1">
+            <Route path="/restaurants" element={<Restaurants />} />
+            <Route path="/restaurants/:id/details" element={<RestaurantDetails />} />
 
-                    <Routes>
-                        <Route path='/' element={<Home />} />
-                        <Route path='/register' element={<Register />} />
-                        <Route path='/login' element={<Login />} />
-                        <Route path='/restaurants' element={<Restaurants />} />
-                        <Route path='/restaurants/:id/details' element={<RestaurantDetails />} />
-                        <Route path='/profile' element={<MyProfile />} />
-                        <Route path='/logout' element={<Logout />} />
-                        <Route path='/add-restaurant' element={<AddRestaurant />} />
-                        <Route path='/my-restaurants' element={<MyRestaurants />} />
-                        <Route path='/restaurants/:id/edit' element={<EditRestaurant />} />
-                        <Route path="*" element={<NotFound />} />
-                    </Routes>
+            <Route element={<AuthGuard />}>
+              <Route path="/profile" element={<MyProfile />} />
+              <Route path="/logout" element={<Logout />} />
+              <Route path="/add-restaurant" element={<AddRestaurant />} />
+              <Route path="/my-restaurants" element={<MyRestaurants />} />
+              <Route path="/restaurants/:id/edit" element={<EditRestaurant />} />
+            </Route>
 
-                </main>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
 
-                <Footer />
-
-            </div>
-        </UserProvider>
-    )
+        <Footer />
+      </div>
+    </UserProvider>
+  )
 }
 
 export default App
