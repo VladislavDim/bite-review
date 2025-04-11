45d67270d9acf3eeb4dca2d4f12684736cfce260
Dimitrov Vladislav <64010169+VladislavDim@users.noreply.github.com>
2025-04-11 22:34:09 +0300

    Update TextInput to allow data persistence via controlled props

--- client/src/components/ui/TextInput.jsx
+++ client/src/components/ui/TextInput.jsx
@@ -5,6 +5,9 @@ export default function TextInput({
     type = "text",
     placeholder = "",
     required = true,
+    error,
+    value,
+    onChange,
 }) {
     return (
         <div>
@@ -19,10 +22,13 @@ export default function TextInput({
                 name={name}
                 placeholder={placeholder}
                 required={required}
+                value={value}
+                onChange={onChange}
                 className="w-full bg-transparent border-b-2 border-[#E9762B] text-gray-800 
                            placeholder-gray-400 focus:outline-none focus:ring-1 
                            focus:ring-orange-200 focus:border-[#E9762B] transition-all duration-300"
             />
+            {error && <p className="text-red-500 text-sm mt-1">{error}</p>}
         </div>
     );
 }
