6a4d03bd32c2f041ca20478fcd472243e05b147a
Dimitrov Vladislav <64010169+VladislavDim@users.noreply.github.com>
2025-04-15 22:39:18 +0300

    Configure MongoDB connection using Mongoose and .env.

--- src/server.js
+++ src/server.js
@@ -1,14 +1,17 @@
-// server.js
-require('dotenv').config();
-const express = require('express');
+// src/server.js
+import dotenv from 'dotenv';
+import express from 'express';
+import connectDB from './config/db.js';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
+connectDB();
+
 app.use(express.json());
 
-// Test route
 app.get('/', (req, res) => {
   res.send('BiteReview API is running!');
 });
