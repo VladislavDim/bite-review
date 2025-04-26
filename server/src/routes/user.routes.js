ca45e02f3d501487a111fc2895058bfc7b26c9dc
Dimitrov Vladislav <64010169+VladislavDim@users.noreply.github.com>
2025-04-26 08:47:21 +0300

    feat(user): add route to fetch user profile by ID

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getAllUsers, createUser } from '../controllers/user.controller.js';
+import { getAllUsers, createUser, getUserById } from '../controllers/user.controller.js';
 
 const router = express.Router();
 
 router.get('/', getAllUsers);
+router.get('/:id', getUserById);
 router.post('/', createUser);
 
 export default router;
