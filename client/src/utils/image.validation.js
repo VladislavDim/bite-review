35044b4e59efed8296e93b14e96dec61bceedf4a
Dimitrov Vladislav <64010169+VladislavDim@users.noreply.github.com>
2025-05-02 14:08:46 +0300

    Add image validation utility with allowed types and max size constants

---
+++ client/src/utils/image.validation.js
@@ -0,0 +1,3 @@
+export const ALLOWED_IMAGE_TYPES = ['image/jpeg', 'image/png', 'image/webp'];
+export const MAX_IMAGE_SIZE_MB = 5;
+export const MAX_IMAGE_SIZE_BYTES = MAX_IMAGE_SIZE_MB * 1024 * 1024;
