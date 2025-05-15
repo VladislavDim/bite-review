e815b707a83f862c76b873026f1c0d0cf5e50dac
Dimitrov Vladislav <64010169+VladislavDim@users.noreply.github.com>
2025-05-15 12:20:15 +0300

    Refactor user API hook: memoize getUser with useCallback

--- client/src/api/userApi.js
+++ client/src/api/userApi.js
@@ -1,11 +1,13 @@
+import { useCallback } from "react";
 import request from "../utils/request";
+
 const baseUrl = `${import.meta.env.VITE_APP_SERVER_URL}/api/users`;
 
 export const useGetUserById = () => {
-    const getUser = async (userId) => {
+    const getUser = useCallback(async (userId) => {
         const response = await request.get(`${baseUrl}/${userId}`);
         return response;
-    };
+    }, []);
 
     return { getUser };
 };
