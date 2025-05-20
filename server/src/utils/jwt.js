267759ffec59bba09d0d793e77948cf994b08853
Dimitrov Vladislav <64010169+VladislavDim@users.noreply.github.com>
2025-05-20 10:17:47 +0300

    Refactor generateToken to use standard JWT claims

--- server/src/utils/jwt.js
+++ server/src/utils/jwt.js
@@ -8,8 +8,13 @@ import jwt from 'jsonwebtoken';
 
 export const generateToken = (user) => {
     return jwt.sign(
-        { _id: user._id, email: user.email },
+        {
+            sub: user._id,
+            role: user.role
+        },
         process.env.JWT_SECRET,
-        { expiresIn: process.env.JWT_EXPIRES_IN || '2d' }
+        {
+            expiresIn: process.env.JWT_EXPIRES_IN || '2d'
+        }
     );
 };
