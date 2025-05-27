0a218304224d173a6a92559c4ecf4cf5c05e2c9d
Dimitrov Vladislav <64010169+VladislavDim@users.noreply.github.com>
2025-05-27 22:25:06 +0300

    refactor(upload): switch avatar upload to memory storage for Cloudinary integration

--- server/src/middlewares/user.avatar.upload.middleware.js
+++ server/src/middlewares/user.avatar.upload.middleware.js
@@ -1,10 +1,9 @@
-import paths from '../utils/paths.js';
-import { createUploadMiddleware } from './upload.factory.middleware.js';
 import {
     isValidImageFile,
     ALLOWED_AVATAR_IMAGE_TYPES,
     MAX_AVATAR_IMAGE_SIZE_BYTES,
 } from '../utils/image.rules.js';
+import { createUploadMiddleware } from './upload.factory.middleware.js';
 
 const avatarFileFilter = (_, file, cb) => {
     if (isValidImageFile(file, ALLOWED_AVATAR_IMAGE_TYPES)) {
@@ -18,7 +17,7 @@ const avatarFileFilter = (_, file, cb) => {
 };
 
 const uploadAvatar = createUploadMiddleware({
-    uploadDir: paths.avatarUploads,
+    useMemory: true, 
     limits: { fileSize: MAX_AVATAR_IMAGE_SIZE_BYTES },
     fileFilter: avatarFileFilter,
 });
